6dd1f7ce8878ac2657ea793340d097f0db590ee8
Lucy <13240016260@163.com>
2021-01-18 13:19:05 +0800

    Delete CCKS-Mrc directory

--- CCKS-Mrc/get_data.py
+++
@@ -1,6 +0,0 @@
-from preprocessing.data_processor import read_squad_data
-
-if __name__ == "__main__":
-    read_squad_data("/home/LAB/liqian/test/game/Ori_graph/CCKS-Mrc/data/squad-like_all_train_data.json", "/home/LAB/liqian/test/game/Ori_graph/CCKS-Mrc/data/",is_training=True)
-
-
