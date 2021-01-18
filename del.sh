ba7258a76e534b147a65bc49a5138b0a780f483a
Lucy <13240016260@163.com>
2021-01-18 13:23:33 +0800

    Delete del.sh

--- del.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/sh
-rm -if CCKS-Cls/test_output/* CCKS-Cls/pybert/output/figure/*.png CCKS-Cls/pybert/output/figure/*.json CCKS-Cls/pybert/output/log/* CCKS-Cls/pybert/dataset/*
-rm -if CCKS-Mrc/data/*
-
